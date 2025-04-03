38d0cbb730ba9278e80a0cc8faf5ce4987f727ef
Solareclipse04 <lera.pokatskaya.05@mail.ru>
2025-04-03 15:49:46 +0700

    fgdfgd

--- ProgramTechnology/GuessNumber/GuessNumberGame.cs
+++ ProgramTechnology/GuessNumber/GuessNumberGame.cs
@@ -19,6 +19,7 @@ namespace GuessNumber
         {
             _iNumberGenerator = iNumberGenerator;
             _iInputAction = iInputAction;
+            int a;
         }
         public void Run()
         {
